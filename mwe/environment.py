389803c93a7da30f5d5a70c8e93dae6c36d50568
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2021-08-12 18:02:42 +0100

    Basic gridworld environment setup

--- mwe/environment.py
+++ mwe/environment.py
@@ -1,10 +1,24 @@
 from typing import List
 from data import Action, Observation
+import drawSvg as draw
+import random
+
+
+def rand_hex_col() -> str:
+    r = lambda: random.randint(0, 255)
+    return '#%02X%02X%02X%02X' % (r(), r(), r(), 255)
 
 
 class Environment:
     def __init__(self):
-        pass
+        self._size = 5
+        self._board = [
+            '#', '.', '#', '#', '#',
+            '#', '.', '#', '.', '#',
+            '#', '.', '#', '.', '#',
+            '#', '.', '.', '.', '.',
+            '#', '.', '#', '#', '#'
+        ]
 
     def reset(self) -> Observation:
         # todo
@@ -12,5 +26,26 @@ class Environment:
         
 
     def update(self, act: Action) -> Observation:
+        # todo
+        return Observation([0.0])
+        
+
+    def draw(self) -> None:
+        drawing_size = 200
+        tile_size = drawing_size / self._size
+
+        d = draw.Drawing(drawing_size, drawing_size)
+
+        for x in range(self._size):
+            for y in range(self._size):
+                idx = (self._size - y - 1) * self._size + x
+                tile = self._board[idx]
+                
+                col = rand_hex_col() if tile == '#' else "#00000000"
+                r = draw.Rectangle(x * tile_size, y * tile_size, tile_size, tile_size, fill=col)
+                d.append(r)
+
+        d.saveSvg('example.svg')
+
         # todo
         return Observation([0.0])
--- mwe/simulation.py
+++ mwe/simulation.py
@@ -7,10 +7,16 @@ def main() -> None:
     agent = Agent()
 
     obs = env.reset()
+    env.draw()
 
-    for step in range(0, 100):
+    for step in range(0, 1):
         act = agent.update(obs)
         obs = env.update(act)
+
+        #rand_col = rand_hex_col()
+        #r = draw.Rectangle(-80,0,40,50, fill=rand_col)
+        #d.append(r)
+        #d.savePng(r)
 
 
 if __name__ == '__main__':
